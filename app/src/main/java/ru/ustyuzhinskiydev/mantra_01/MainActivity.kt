fb360306097a7f266022555290111ecea2fb671d
Gennadii Ustiuzhinskii <mampormin@gmail.com>
2021-04-15 22:30:56 +0300

    параметр solid в shape_drawable не влияет на цвет кнопки button (в land/activity_main.xml)

--- app/src/main/java/ru/ustyuzhinskiydev/mantra_01/MainActivity.kt
+++ app/src/main/java/ru/ustyuzhinskiydev/mantra_01/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.ustyuzhinskiydev.mantra_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Я буду андроид разработчиком!", Toast.LENGTH_LONG).show()
+        }
     }
 }
